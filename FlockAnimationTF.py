ac9a889804b29df7875e903899473b60637a9470
Ari <ari.j.herman@gmail.com>
2017-05-21 23:08:00 -0700

    Cleaned up extraneous code

--- FlockAnimationTF.py
+++ FlockAnimationTF.py
@@ -98,6 +98,53 @@ d = differences(diffs)
 u = uUpdate(q,p)
 deriv = differentiate(g)
 
+"""
+Some kind of issue here with using linspace...
+"""
+
+#####################
+# Generate trajectory
+#####################
+if dim == 2:
+    # Pick a path for gamme agent
+    gamma_path = input("Select path for gamma agent ['circle','eight']: ")
+    
+    if gamma_path == "circle":
+        x=tf.cos(tf.constant(np.linspace(0,2*np.pi,num_iters)))
+        y=tf.sin(tf.constant(tf.linspace(0,2*np.pi,num_iters)))
+
+    elif gamma_path == "eight":
+        x=tf.cos(tf.linspace(0,2*np.pi,num_iters))
+        y=tf.sin(tf.linspace(0,4*np.pi,num_iters))
+    
+    else:
+        assert(False)
+
+    q_g=tf.stack((x,y),axis=1) 
+    p_g=tf.stack((differentiate(x),differentiate(y)),axis=1)
+
+if dim ==3:
+    # Pick a path for gamma agent
+    gamma_path = input("Select path for gamma agent ['circle','wild']: ")
+    
+    if gamma_path == "circle":    
+        # Gamma agent (moves in a circle)
+        x=tf.cos(np.linspace(0,2*np.pi,num_iters))
+        y=tf.sin(np.linspace(0,2*np.pi,num_iters))
+        z=tf.zeros(num_iters)
+    
+    elif gamma_path == "wild":
+        x=tf.cos(np.linspace(0,2*np.pi,num_iters))
+        y=tf.cos(np.linspace(0,4*np.pi,num_iters))
+        z=tf.sin(np.linspace(0,8*np.pi,num_iters))
+    
+    else:
+        assert(False)
+
+    q_g=tf.stack((x,y,y),axis=1)
+    p_g=tf.stack((differentiate(x),differentiate(y),differentiate(z)),axis=1)
+
+
 ##################
 # Begin tf session
 ##################
@@ -118,125 +165,6 @@ tf_d = sess.run(d)
 tf_u = sess.run(u)
 tf_deriv = sess.run(deriv)
 
-##################
-# Useful functions
-##################
-
-def np_sig_norm(z): # Sigma norm
-    return (np.sqrt(1+eps*np.sum(z**2,axis=2).reshape((num_boids,num_boids,1)))-1)/eps
-
-def np_sig_grad(z,norm=None,eps=eps): # Gradient of sigma norm
-    if type(norm) == "NoneType":
-        return z/(1+eps*sig_norm(z))
-    else:
-        return z/(1+eps*norm)
-
-def np_rho_h(z):
-    return  np.logical_and(z>=0,z<h)+np.logical_and(z<=1,z>=h)*(0.5*(1+np.cos(np.pi*(z-h)/(1-h))))
-
-def np_phi(z):
-    return 0.5*((a+b)*sig_grad(z+c,1)+(a-b))
-
-
-def np_phi_a(z):
-    return rho_h(z/r_a)*phi(z-d_a)
-    
-def np_differences(q):
-    return q[:,None,:] - q
-
-def np_uUpdate(q,p):
-    diff=differences(q)
-    norms = sig_norm(diff)
-    diffp=differences(p)
-    return np.sum(phi_a(norms)*sig_grad(diff,norms),axis=0)+np.sum(rho_h(norms/r_a)*diffp,axis=0)
-
-def np_differentiate(v):
-    dv = v.copy()
-    dv[1:]-=v[:-1]
-    return dv/dt
-
-
-np_diff = tf_diff
-np_q = tf_q
-np_p = tf_p
-np_g = tf_g
-np_norm = np_sig_norm(np_diff)
-np_grad = np_sig_grad(np_diff,np_norm)
-np_r = np_rho_h(np_diff)
-np_p = np_phi(np_diff)
-np_pa = np_phi_a(np_diff)
-np_d = np_differences(np_diff)
-np_u = np_uUpdate(np_q,np_p)
-np_deriv = np_differentiate(tf_g)
-
-print("diff")
-print(np_diff-tf_diff)
-print("norm")
-print(np_norm-tf_norm)
-print("grad")
-print(np_grad-tf_grad)
-print("rho")
-print(np_r-tf_r)
-print("phi")
-print(np_p-tf_p)
-print("phi_a")
-print(np_pa-tf_pa)
-print("differences")
-print(np_d-tf_d)
-print("update")
-print(np_u-tf_u)
-print("derivs")
-print(np_deriv-tf_deriv)
-
-######################
-
-def differentiate(v):
-    dv = v.copy()
-    dv[1:]-=v[:-1]
-    return dv/dt
-
-## Generate trajectory
-######################
-#if dim == 2:
-#    # Pick a path for gamme agent
-#    gamma_path = input("Select path for gamma agent ['circle','eight']: ")
-#    
-#    if gamma_path == "circle":
-#        x=tf.cos(np.linspace(0,2*np.pi,num_iters))
-#        y=tf.sin(np.linspace(0,2*np.pi,num_iters))
-#
-#    elif gamma_path == "eight":
-#        x=tf.cos(np.linspace(0,2*np.pi,num_iters))
-#        y=tf.sin(np.linspace(0,4*np.pi,num_iters))
-#    
-#    else:
-#        assert(False)
-#
-#    q_g=tf.stack((x,y),axis=1) 
-#    p_g=tf.stack((differentiate(x),differentiate(y)),axis=1)
-#
-#if dim ==3:
-#    # Pick a path for gamma agent
-#    gamma_path = input("Select path for gamma agent ['circle','wild']: ")
-#    
-#    if gamma_path == "circle":    
-#        # Gamma agent (moves in a circle)
-#        x=tf.cos(np.linspace(0,2*np.pi,num_iters))
-#        y=tf.sin(np.linspace(0,2*np.pi,num_iters))
-#        z=tf.zeros(num_iters)
-#    
-#    elif gamma_path == "wild":
-#        x=tf.cos(np.linspace(0,2*np.pi,num_iters))
-#        y=tf.cos(np.linspace(0,4*np.pi,num_iters))
-#        z=tf.sin(np.linspace(0,8*np.pi,num_iters))
-#    
-#    else:
-#        assert(False)
-#
-#    q_g=tf.stack((x,y,y),axis=1)
-#    p_g=tf.stack((differentiate(x),differentiate(y),differentiate(z)),axis=1)
-#
-#
 ############
 ## Animation
 ############
